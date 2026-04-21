ecbbca15cc1b969ed3b059151d57d135082d9386
Goodness Obi <goodnessobi@gmail.com>
2026-04-21 13:37:20 +0100

    feat: display posts

---
+++ resources/js/pages/Posts/Index.vue
@@ -0,0 +1,25 @@
+<template>
+   <AppLayout title="Posts">
+      <Container>
+         <ul class="divide-y divide-gray-200 bg-white">
+            <li v-for="post in posts" :key="post.id" class="px-2 py-4">
+               <span class="font-bold text-lg" >
+                  {{ post.title }}
+               </span>
+            </li>
+         </ul>
+      </Container>
+   </AppLayout>
+</template>
+
+
+<script setup>
+import Container from '@/Components/Container.vue';
+import AppLayout from '@/Layouts/AppLayout.vue';
+
+defineProps({
+   posts: Array,
+});
+
+
+</script>
