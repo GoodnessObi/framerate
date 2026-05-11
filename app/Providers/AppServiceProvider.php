edf8231efdea650444fff230d0b5c8041788a14e
Goodness Obi <goodnessobi@gmail.com>
2026-05-11 12:52:57 +0100

    feat: create comment method

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,6 +28,7 @@ class AppServiceProvider extends ServiceProvider
         $this->configureDefaults();
         JsonResource::withoutWrapping();
         Model::preventLazyLoading();
+        Model::unguard();
     }
 
     /**
---
+++ tests/Feature/Controllers/CommentController/StoreTest.php
@@ -0,0 +1,48 @@
+<?php
+
+use App\Models\User;
+use App\Models\Post;
+use App\Models\Comment;
+
+it('can store a comment', function () {
+    $user = User::factory()->create();
+    $post = Post::factory()->create();
+
+    $this->actingAs($user)->post(route('posts.comments.store', $post), [
+        'body' => 'This is a test comment',
+    ]);
+
+    $this->assertDatabaseHas(Comment::class, [
+        'body' => 'This is a test comment',
+        'user_id' => $user->id,
+        'post_id' => $post->id,
+    ]);
+});
+
+
+it('redirects to the post show page', function () {
+    $post = Post::factory()->create();
+
+    $this->actingAs(User::factory()->create())
+        ->post(route('posts.comments.store', $post), [
+            'body' => 'This is a test comment',
+        ])
+        ->assertRedirect(route('posts.show', $post));
+});
+
+
+it('requires a valid body', function ($value) {
+    $post = Post::factory()->create();
+
+    $this->actingAs(User::factory()->create())
+        ->post(route('posts.comments.store', $post), [
+            'body' => $value,
+        ])
+        ->assertInvalid('body');
+})->with([
+    null,
+    1,
+    1.5,
+    true,
+    str_repeat('a', 2501),
+]);
